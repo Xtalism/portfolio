9c508d68ca640ebde48bde863ddff412902c5984
Xtalism <manuel_lego1234@hotmail.com>
2024-08-02 21:37:37 -0600

    Navigation added

--- script.js
+++ script.js
@@ -35,14 +35,48 @@ document.addEventListener("DOMContentLoaded", function () {
           window.open("https://github.com/Xtalism/object-detection", "_blank");
           break;
         case "portfolio":
-          window.open("https://www.youtube.com/watch?v=UqHILyzcULE&t=1817s&ab_channel=HowtoWebDev", "_blank");
+          window.open(
+            "https://www.youtube.com/watch?v=UqHILyzcULE&t=1817s&ab_channel=HowtoWebDev",
+            "_blank"
+          );
           break;
         case "robotic-arm":
-          window.open("https://github.com/Xtalism/aero-pid-controller", "_blank");
+          window.open(
+            "https://github.com/Xtalism/aero-pid-controller",
+            "_blank"
+          );
           break;
         default:
           console.log("No matching link found.");
       }
     });
   });
+});
+
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((link) => {
+        link.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
 });
